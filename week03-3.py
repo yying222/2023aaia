cf34b5e00e3ed7a67d062397b6495ffd4f1a7ede
yying222 <144758367+yying222@users.noreply.github.com>
2023-09-26 11:53:29 +0800

    week03

---
+++ week03-3.py
@@ -0,0 +1,6 @@
+#print(' 1x1=1 1x2=2 1x3=3 1x4=4 1x5=5 1x6=6')
+#print(' 2x1=2 2x2=4 2x3=6 2x4=8 2x5=10 2x6=12')
+for i in range(9):
+  for j in range(9):
+    print(i,'x', j , sep='' , end=' ')
+  print()
