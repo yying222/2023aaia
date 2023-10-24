348c6add7e9c6e30ba13749a319dc2b1d7065825
yying222 <angelwang201225@gmail.com>
2023-10-24 10:16:36 +0800

    week07

---
+++ week07/week07-1.py
@@ -0,0 +1,13 @@
+#
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n<=0 : #負數一定是錯的，0也是錯的
+            return False
+
+        while n>1 :
+            if n % 2 !=0 : #有餘數，就失敗了
+                return False #失敗
+
+            n = n // 2 #16//2得到8，數字變小
+
+        return True
