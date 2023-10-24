0eb7ee2242dc5098a9f69fb4a644a560897ea9d6
yying222 <angelwang201225@gmail.com>
2023-10-24 11:45:38 +0800

    week07

---
+++ week07/week07-5.py
@@ -0,0 +1,1 @@
+# week07-5 字串、字典s = "abcdabcd" # str字串d = {} #dict字典for c in s:  d[c] = 1  print('現在讀到的字母',c,"字典變成", d)
---
+++ week07/week07-6.py
@@ -0,0 +1,1 @@
+# week07-6 樓上改過來s = "abcdabcd" # str字串d = {} #dict字典for c in s:  if c in d:    d[c] = d[c] + 1  else:    d[c] = 1  print('現在讀到的字母',c,"字典變成", d)
