87dfc01bc78e7c040790638c5d568d96dfd2fb3f
yying222 <144758367+yying222@users.noreply.github.com>
2024-01-09 11:51:30 +0800

    Add files via upload

---
+++ week11/week11-1.py
@@ -0,0 +1,16 @@
+
+
+a = list(map(int, input().split() ))
+
+
+N = len(a)
+for k in range(N):
+    for i in range(N-1):
+    	if a[i] > a[i+1]:
+     		a[i], a[i+1] = a[i+1], a[i]
+ 
+ 
+for i in range(N):
+	print( '', a[i], end='' )
+	if i%10 == 9 and i!=99:
+		print()		
